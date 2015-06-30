cb90dc292c3e89e52462a0e2528205025f88f68e
Mikela Clemmons <glassresistor@gmail.com>
2015-06-30 12:26:50 -0700

    moved from form to view

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -1,20 +1,5 @@
-import csv
 from django import forms
-from newsletter import models
 
 class UploadSubscribersInput(forms.Form):
     csv_file = forms.FileField()
     date = forms.DateField()
-
-    def save(self):
-        records = csv.reader(self.cleaned_data["csv_file"])
-        records.__next__()
-        for line in records:
-            subscriber = model.Subscribers()
-            subscriber.date = self.cleaned_data["date"]
-            subscriber.bounce = line[0]
-            subscriber.email = line[1]
-            truthy = { 'TRUE': True, 'FALSE' : False }
-            subscriber.active = truthy.get(line[2])
-            subscriber.domain = line[3]
-            subscriber.save()
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
-from newsletter import forms
+import csv
 
+from django.shortcuts import render
+from newsletter import forms, models
+
+from django.core.urlresolvers import reverse
 from django.views.generic.edit import FormView
 
 
 class UploadSubscribersView(FormView):
     template_name = 'newsletter/subscribers_upload_view.html'
     form_class = forms.UploadSubscribersInput
+    success_url = '/'
+
+    def form_valid(self, form):
+        print(self.request.FILES)
+        records = csv.reader(self.request.FILES["csv_file"])
+        records.__next__()
+        truthy = { 'TRUE': True, 'FALSE' : False }
+        for line in records:
+            subscriber = models.Subscribers()
+            subscriber.date = form.cleaned_data["date"]
+            subscriber.bounce = line[0]
+            subscriber.email = line[1]
+            subscriber.active = truthy.get(line[2])
+            subscriber.domain = line[3]
+            subscriber.save()
+        return super(UploadSubscribersView, self).form_valid(form)
