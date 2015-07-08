cf1b30007ee573fa4d44a1ee8b64870a0e391db6
Mikela Clemmons <glassresistor@gmail.com>
2015-07-07 19:24:21 -0700

    added reports, excluded ones that rely on difference

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,41 +1,42 @@
-from functools import wraps
 from django.db import models
 from django.db.models import Q
-
-
-def memoize(func):
-    @wraps(func)
-    def memoizer(*args, **kwargs):
-        if not hasattr(func, '_cache'):
-            func._cache = func(*args, **kwargs)
-        return func._cache
-    return memoizer
 
 
 class Week(models.Model):
     date = models.DateField(unique=True)
     complete = models.BooleanField(default=False)
 
-    @memoize
     def subscribers_count(self):
         return self.subscriber_set.all().count()
 
-    @memoize
     def inactive_subscribers(self):
         return self.subscriber_set.filter(active=True).filter(bounces__lte=1)
 
-    @memoize
     def inactive_subscribers_count(self):
         return self.inactive_subscribers().count()
 
-    @memoize
     def active_subscribers(self):
         return self.subscriber_set.filter(active=True).filter(bounces__lte=1)
 
-    @memoize
     def active_subscribers_count(self):
         return self.active_subscribers().count()
 
+    def previous_week(self):
+        pre = self.get_previous_by_date()
+        return pre
+
+    def change_in_subscribers(self):
+        pre = self.previous_week()
+        return self.subscribers_count() - pre.subscribers_count()
+
+    def new_subscribers(self):
+        pre = self.previous_week().subscriber_set.all()
+        qs = self.subscriber_set.exclude(email__in=pre.values('email'))
+        return qs.count()
+
+    def change_in_active_subscribers(self):
+        pre = self.previous_week()
+        return self.active_subscribers_count() - pre.active_subscribers_count()
 
 class Subscriber(models.Model):
     email = models.CharField(max_length=40)
