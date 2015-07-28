3d8f49f056444554177b6ccc3c866c85fae8b390
Mikela Clemmons <glassresistor@gmail.com>
2015-07-28 11:19:16 -0700

    included custom admin in register

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -5,7 +5,7 @@ from newsletter import models
 class SubscriberAdmin(admin.ModelAdmin):
     raw_id_fields = ("signup",)
 
-admin.site.register(models.Subscriber)
+admin.site.register(models.Subscriber, SubscriberAdmin)
 admin.site.register(models.Week)
 admin.site.register(models.Signup)
 
