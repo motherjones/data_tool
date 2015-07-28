7f1cd7c15b23acfd2020582b00f224f7febb3aab
Mikela Clemmons <glassresistor@gmail.com>
2015-07-28 11:17:54 -0700

    made admin use the raw id so admin doesnt crash

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from newsletter import models
 
+class SubscriberAdmin(admin.ModelAdmin):
+    raw_id_fields = ("signup",)
+
 admin.site.register(models.Subscriber)
 admin.site.register(models.Week)
 admin.site.register(models.Signup)
