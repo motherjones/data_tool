e8eb9a3f227733910bfaa94796e289ce2608cdd5
Mikela Clemmons <glassresistor@gmail.com>
2015-11-11 14:32:27 -0800

    removed pygal

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -5,6 +5,8 @@ from newsletter import views
 urlpatterns = [
     url(r'^subscribers-upload$', views.UploadSubscribersView.as_view(),
         name='subscribers-upload'),
+    url(r'^query-upload$', views.UploadQueryView.as_view(),
+        name='query-upload'),
     url(r'^subscribers-report/(?P<pk>\d+)/', views.ActiveSubscribersView.as_view(), name='subscribers-report'),
     url(r'^subscribers-report/', views.WeekListView.as_view(), name='subscribers-report-list'),
     url(r'^signups-report/', views.SignupsReportView.as_view(), name='signups-report'),
